7ebabf0ff065aa2e09f6d64e4c91283862083715
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-10-12 14:15:59 +0530

    added the banners and changed the meta links and icons

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
 import Header1 from "@/components/Header1";
 import Header2 from "@/components/Header2";
 import Header3 from "@/components/Header3";
+import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>
+          OYO: India&apos;s Best Hotel Booking Site for Sanitized Stays
+        </title>
+        <link rel="icon" href="/icon.png" />
+      </Head>
       <Header1 />
       <Header2 />
       <Header3 />
+      <div className="mx-20 my-14">
+        <div className="mb-14">
+          <Image
+            src={"/banner1.avif"}
+            alt="banner1"
+            width={200}
+            height={200}
+            className="h-80 w-full"
+          />
+        </div>
+        <div className="mb-14">
+          <Image
+            src={"/banner2.avif"}
+            alt="banner2"
+            width={200}
+            height={200}
+            className="h-40 w-full"
+          />
+        </div>
+      </div>
     </div>
   );
 }
