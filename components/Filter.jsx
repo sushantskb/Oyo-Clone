cce5aa606ef370edaf62e665b9136f2fb22ee66e
sushantskb <sushantsrk2001@gmail.com>
2024-12-14 22:23:02 +0700

    integrated the fetch all facilities api

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Filter = () => {
+  const [list, setList] = useState([]);
+
+  const fetchFacilities = async () => {
+    try {
+      const { data } = await axios.get(`/api/facilities`);
+      if (data?.facilites) {
+        setList(data.facilites);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFacilities();
+  }, []);
+
   return (
     <div className="border-2 border-red-500 rounded-md m-5 h-auto py-10 px-3">
       <label htmlFor="price" className="text-xl mr-3 font-bold">
@@ -11,14 +29,18 @@ const Filter = () => {
 
       <div className="my-10">
         <h3 className="text-xl font-bold my-3">Filter by facilities:</h3>
-        <p className="flex items-center my-3">
-          <label htmlFor="checkbox">Free Wifi</label>
-          <input type="checkbox" name="checkbox" className="w-5 h-5 ml-3" />
-        </p>
-        <p className="flex items-center my-3">
-          <label htmlFor="checkbox">Free Wifi</label>
-          <input type="checkbox" name="checkbox" className="w-5 h-5 ml-3" />
-        </p>
+        {list?.map((fac) => (
+          <p key={fac.name} className="grid grid-cols-4 my-3">
+            <label htmlFor="checkbox" className="col-span-2">
+              {fac.name}
+            </label>
+            <input
+              type="checkbox"
+              name="checkbox"
+              className="w-5 h-5 ml-3 col-span-1"
+            />
+          </p>
+        ))}
       </div>
     </div>
   );
